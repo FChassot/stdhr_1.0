17042545eaf71b28ee503c09c9026f6f0ed6ff4a
Frederic Chassot <Frederic Chassot>
2016-04-19 23:21:23 +0200

    Initial commit

---
+++ app/src/main/java/hesso/mas/spatio_temporaldigitalheritagebrowsing/MainActivity.java
@@ -0,0 +1,13 @@
+package hesso.mas.spatio_temporaldigitalheritagebrowsing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
