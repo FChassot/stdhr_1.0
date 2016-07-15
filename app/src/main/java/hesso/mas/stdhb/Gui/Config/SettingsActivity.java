3899a1e5cf5e6d1bcda3821e67586df79203154b
chf <frederic.chassot@gmail.com>
2016-07-15 09:50:42 +0200

    GpsServices und GpsLocationListener

---
+++ app/src/main/java/hesso/mas/stdhb/Gui/Config/SettingsActivity.java
@@ -0,0 +1,34 @@
+package hesso.mas.stdhb.Gui.Config;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Button;
+
+import hesso.mas.stdhb.Gui.MainActivity;
+import hesso.mas.stdhbtests.R;
+
+public class SettingsActivity extends AppCompatActivity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        Button mBtnSave = (Button)findViewById(R.id.btnSave);
+
+        // Positionner un listener sur ce bouton
+        mBtnSave.setOnClickListener((OnClickListener) this);
+
+    }
+
+    // Méthode déclenchée par le listener lorsqu'un appui sur le bouton se produit
+    public void onClick(View view){
+        if (view.getId()==R.id.btnSave){ // C'est notre bouton ? oui, alors affichage d'un message
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
