8563848411d4d446cb9a86fb8e6a07fcc5046dfe
chf <frederic.chassot@gmail.com>
2016-07-15 19:03:21 +0200

    Radar Einbau

--- app/src/main/java/hesso/mas/stdhb/Gui/Radar/DisplayActivity.java
+++ app/src/main/java/hesso/mas/stdhb/Gui/Radar/DisplayActivity.java
@@ -11,21 +11,35 @@ import hesso.mas.stdhbtests.R;
 
 public class DisplayActivity extends AppCompatActivity {
 
+    RadarView mRadarView = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setContentView(R.layout.activity_display);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        mRadarView = (RadarView) findViewById(R.id.radarView);
+        mRadarView.setShowCircles(true);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Mon test", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+        mRadarView.startAnimation();
+    }
+
+    public void stopAnimation(View view) {
+        if (mRadarView != null) mRadarView.stopAnimation();
+    }
+
+    public void startAnimation(View view) {
+        if (mRadarView != null) mRadarView.startAnimation();
     }
 
 }
