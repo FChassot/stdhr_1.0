3bd62c18e558f7abbf33d9eb237c89e9976470f9
chf <frederic.chassot@gmail.com>
2016-09-10 09:40:35 +0200

    EnumClientServerCommunication

--- app/src/main/java/hesso/mas/stdhb/Base/Models/Enum/EnumClientServerCommunication.java
+++ app/src/main/java/hesso/mas/stdhb/Base/Models/Enum/EnumClientServerCommunication.java
@@ -3,13 +3,12 @@ package hesso.mas.stdhb.Base.Models.Enum;
 /**
  * Created by chf on 25.08.2016.
  *
- *
+ * This enum represents the different technologies who are used for the
+ * communication between the client and the server.
  */
 public enum EnumClientServerCommunication {
 
-    UNDEFINED(0), RDF4J(1), ANDROJENA(2), REST(3), OKHTTP(4), ;
-
-    //RETROFIT, SPRING, VOLLEY, SOAP
+    UNDEFINED(0), ANDROJENA(1), RDF4J(2), REST(3), RESTOKHTTP(4), ;
 
     private Integer value ;
 
