2ae6258a1525384d8d7d2b0debe9641338a2d980
chf <frederic.chassot@gmail.com>
2016-09-29 09:37:47 +0200

    Ersten Unittests

---
+++ app/src/test/java/hesso/mas/stdhbtests/RadarUnitTest.java
@@ -0,0 +1,16 @@
+package hesso.mas.stdhbtests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by chf on 28.09.2016.
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class RadarUnitTest {
+    @Test
+    public void calculate_distance_between_two_points_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
