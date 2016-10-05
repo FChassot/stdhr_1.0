4f94c09510bac9541c30a5e5b200b4dd7b60b145
chf <frederic.chassot@gmail.com>
2016-10-05 12:05:48 +0200

    IOC Container Einbau

---
+++ app/src/main/java/hesso/mas/stdhb/DataAccess/QueryBuilder/Literal1.java
@@ -0,0 +1,11 @@
+package hesso.mas.stdhb.DataAccess.QueryBuilder;
+
+/**
+ * Created by chf on 21.09.2016.
+ */
+public class Literal1 {
+
+    private String mValue;
+
+    public Literal1(String aValue) { mValue = aValue; };
+}
