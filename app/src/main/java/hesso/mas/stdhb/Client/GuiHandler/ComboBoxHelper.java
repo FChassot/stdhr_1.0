6ae4606e877aa61361917be868bee2c2a33e1c77
chf <frederic.chassot@gmail.com>
2016-10-05 11:05:41 +0200

    IOC Container Einbau

---
+++ app/src/main/java/hesso/mas/stdhb/Client/GuiHandler/ComboBoxHelper.java
@@ -0,0 +1,20 @@
+package hesso.mas.stdhb.Client.GuiHandler;
+
+import android.widget.Spinner;
+
+/**
+ * Created by chf on 24.08.2016.
+ *
+ *
+ */
+public class ComboBoxHelper {
+
+    /**
+     *
+     * @param aCbo
+     * @param aEnumeration
+     */
+    public void fillCombo(Spinner aCbo, Enum aEnumeration) {
+
+    };
+}
