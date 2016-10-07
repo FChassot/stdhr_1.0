d0be2242f6faf8a38223fed3c03b3550b9a9007e
chf <frederic.chassot@gmail.com>
2016-10-07 09:30:46 +0200

    Cultural Object Type Selektierung Einbau

---
+++ app/src/main/java/hesso/mas/stdhb/DataAccess/QueryEngine/CitizenDbObject.java
@@ -0,0 +1,56 @@
+package hesso.mas.stdhb.DataAccess.QueryEngine.Response;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by chf on 21.09.2016.
+ */
+public class CitizenDbObject implements Parcelable {
+
+    private Map<String, String> mMap = new HashMap<String, String>();
+
+    // Constructor
+    public CitizenDbObject() {
+    }
+
+    // Constructor
+    private CitizenDbObject(Parcel in) {
+        in.readMap(mMap, getClass().getClassLoader());
+    }
+
+    public void put(String aVariable, String aValue) {
+        mMap.put(aVariable, aValue);
+    }
+
+    public String GetValue(String aVariable) {
+        return mMap.get(aVariable);
+    }
+
+    public void writeToParcel(Parcel aDest, int flags) {
+        aDest.writeMap(mMap);
+    }
+
+    /**
+     * Classes implementing the Parcelable interface must also have a non-null
+     * static field called CREATOR of a type that implements the Parcelable.Creator
+     * interface.
+     */
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public CitizenDbObject createFromParcel(Parcel in) {
+            return new CitizenDbObject(in);
+        }
+
+        public CitizenDbObject[] newArray(int size) {
+            return new CitizenDbObject[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+}
